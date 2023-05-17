cf25feb4159b8da5235629f962c8b7610ab49d12
Frank Collins <fwc3rd@gmail.com>
2023-05-17 09:43:28 -0400

    Create dupe_or_duplicate.c

---
+++ dupe_or_duplicate.c
@@ -0,0 +1,3 @@
+int main () {
+    printf("this will hopefully allow the user to duplicate files on command. If A user is dragging onto a hard drive (press M to move, not duplicate to hard drive\n"); 
+}
