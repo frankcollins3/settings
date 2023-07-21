ce6a27a3dfcea1a71df82d8dc39e76340d8a084e
Frank Collins <fwc3rd@gmail.com>
2023-07-21 08:09:51 -0400

    forgot its PID not PIP [8:09am]

---
+++ PIDsqueak.c
@@ -0,0 +1,7 @@
+// born out of problems in nextJS of finding which prisma instance was running while getting "10 instances warning" and seeing nothing.
+// KILL all instances of anything related to pipsqueak using C or C++
+
+int main () {
+{... 
+  return 777;
+}
